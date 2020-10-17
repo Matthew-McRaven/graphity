07df66d4dac840b5efaf2a902ebf6075f665a25d
Matthew McRaven <mkm302@georgetown.edu>
2020-10-16 17:25:27 -0700

    Add dependencies, enable `pip wheel .` for build.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='graphity',
+      version='0.1.0',
+      description='Research in background independent quantum gravity, also known as quantum graphity.',
+      author='Matthew McRaven',
+      author_email='mkm302@georgetown.edu',
+      packages=['graphity'],
+     )

94193507bd2bc84c4cf3780f3556751d0427fdd6
Matthew McRaven <mkm302@georgetown.edu>
2020-10-17 21:21:31 -0700

    Extend geometric to manipulate NetworkX graphs.
    I have exteneded geometic.py to convert from a tensor object to a graph.
    Inline I included links to documentation.
    
    Please note thata new package, matplotlib, has been added.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='graphity',
-      version='0.1.0.1',
+      version='0.1.0.2',
       description='Research in background independent quantum gravity, also known as quantum graphity.',
       author='Matthew McRaven',
       author_email='mkm302@georgetown.edu',
--- tools/geometric.py
+++ tools/geometric.py
@@ -1,6 +1,14 @@
+# All imports from python standard library. Alphabatized, one per line.
+# Which are empty for now...
+
+# All imports from modules we got from pip. Alphabatized, one per line.
+import matplotlib.pyplot as plt
+import networkx as nx
 import torch
 
-import graphity.environment.reward, graphity.environment.sim
+# All imports from modules we wrote ourselves. Alphabatized, one per line.
+import graphity.environment.reward
+import graphity.environment.sim
 import graphity.agent.markov
 
 def main(args):
@@ -21,16 +29,44 @@ def main(args):
         # Generate a new action and take it.
         action = agent(state)
         state, reward = env.step(action)
+
         # If we run for a long time, be kind and provide proof of forward progress.
         if step % 10_000 == 0:
             print(f"A total of {step} steps have elapsed. Best is {min_reward}.")
+            
         # Record if current state is a new min.
         if reward < min_reward:
             min_reward, min_state = reward, state
 
     print(min_state, min_reward)
+    # Since we may be working on the GPU, we must explictly transfer to CPU before moving to numpy.
+    # Must view(...) changes the dimensions of the reward tensor from 1xnxn to nxn.
+    # Detach deletes any stored gradient information  (important when using machine learning!)
+    # CPU forces tensor to CPU, which is required to convert from a tensor to a numpy array.
+    adj = min_state.view(args.size, -1).detach().cpu().numpy()
+    # Convert from adjacency matrix to NetworkX object.
+    # See documentation for information about the library:
+    #    https://networkx.github.io/documentation/stable/
+    #    https://networkx.github.io/
+    #    https://pypi.org/project/networkx/
+    # If you have questions about what algorithmsare implemented on these graphs, see:
+    #    https://networkx.github.io/documentation/stable/reference/index.html
+    as_graph = nx.from_numpy_matrix(adj)
 
+    # Drawing example taken from:
+    #    https://networkx.github.io/documentation/latest/auto_examples/drawing/plot_weighted_graph.html
+    pos = nx.spring_layout(as_graph) 
+    # Draw nodes & edges.
+    nx.draw_networkx_nodes(as_graph, pos, node_size=700)
+    nx.draw_networkx_edges(as_graph, pos, width=6)
+    # Render the graph to the screen.
+    plt.axis("off")
+    plt.show()
+
+# Python idiom that prevents main() from being called unless this file was called directly from command line.
+# If this file is grabbed via import, main() will not execute automatically.
 if __name__ == "__main__":
+    # Import a library that will let use create a command line parser.
     import argparse
     parser = argparse.ArgumentParser(description='This program generates a random graph of size (size), and randomly toggles edges.')
     parser.add_argument('--size', type=int, default=8,

Fix gradient descent bug.
Fix bug where entire row/column is toggled at a time.

src/graphity/strategy/grad/grad.py
@@ -22,12 +22,8 @@ class gd_sampling_strategy:
         # Pick top transition than most minimizes energy.
         _, index = torch.topk(masked_grad.view(-1), 1, largest=False)
         picked = np.unravel_index(index.item(), masked_grad.shape)
-        action = torch.tensor(picked[0]).view(-1)
-        site = torch.tensor(picked[1:]).view(-1)
-
-        # Must stack along dim=-1 in order to properly join pairs.
-        actions = torch.cat((site, action+2), dim=-1).to(adj.device)
-        return actions, torch.zeros((1,), device=adj.device)
+        site = torch.tensor(picked[:]).view(-1)
+        return site, torch.zeros((1,), device=adj.device)
 
 class softmax_sampling_strategy:
     def __init__(self, grad_fn=None, mask_triu=True): 
@@ -45,18 +41,15 @@ class softmax_sampling_strategy:
         dist = torch.distributions.categorical.Categorical(probs)
         index = dist.sample((1,))
         picked = np.unravel_index(index.item(), masked_grad.shape)
-        action = torch.tensor(picked[0]).view(-1)
-        site = torch.tensor(picked[1:]).view(-1)
+        site = torch.tensor(picked[:]).view(-1)
 
-        # Must stack along dim=-1 in order to properly join pairs.
-        actions = torch.cat((site,action+2), dim=-1).to(adj.device)
         """print("SMGD")
         print(adj)
         print(grad)
         print(index)
         print(actions)
         print()"""
-        return actions, dist.log_prob(index)
+        return site, dist.log_prob(index)
 
 
 class beta_sampling_strategy:
@@ -78,9 +71,6 @@ class beta_sampling_strategy:
         value = self.dist.sample((1,))
         index = torch.argmin((masked_grad-value).abs())
         picked = np.unravel_index(index.item(), masked_grad.shape)
-        action = torch.tensor(picked[0]).view(-1)
-        site = torch.tensor(picked[1:]).view(-1)
+        site = torch.tensor(picked[:]).view(-1)
 
-        # Must stack along dim=-1 in order to properly join pairs.
-        actions = torch.cat((site,action), dim=-1).to(adj.device)
-        return actions, self.dist.log_prob(value)
+        return site, self.dist.log_prob(value)
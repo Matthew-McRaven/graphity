821ae8d108ccfde65bafe02f7b53457125b2d429
Matthew McRaven <mkm302@georgetown.edu>
2021-04-09 18:48:56 -0700

    Improved eq condition.

--- anneal/main.py
+++ anneal/main.py
@@ -61,11 +61,12 @@ class controller:
 		self.available_tasks = [create_task(idx, beta, glass_shape) for idx in range(task_count)]
 		self.epoch = 0
 		self.eq_checks = []
-		self.epoch_stop_at = 1000
-		self.epoch_additional = 40
+		self.forever = 20000
 		self.resume_state =  [None for i in range(task_count)]
-		self.energies =[[] for i in range(task_count)]
-		self.logs = {task:{"trajectories":[]} for task in range(task_count)}
+		self.outer_window_size = 40
+		self.inner_window_size = 10
+		self.sliding_window = [[] for i in range(task_count)]
+		self.energy_list = [[] for i in range(task_count)]
 		self.finalizers = finalizers
 
 	def run(self):
@@ -78,39 +79,54 @@ class controller:
 			]
 
 			# Equilibrium check is expensive and can starve actual work. Don't run too often.
-			if self.epoch % 5 == 0: self.eq_checks.append(in_equilibrium.remote(self.epoch, self.energies, 10))
+			if self.epoch % 20 == 0 and len(self.sliding_window[0]) >= self.outer_window_size: 
+				self.eq_checks.append(in_equilibrium.remote(self.epoch, self.energy_list, self.inner_window_size))
 			ready_refs, self.eq_checks = ray.wait(self.eq_checks, num_returns=1, timeout=0.0)
 			# Stop iterating if any equilibrium checks passed.
-			if any(ray.get(obj) for obj in ready_refs): self.epoch_stop_at = min(self.epoch_stop_at, self.epoch)
+			if any(ray.get(obj) for obj in ready_refs): self.forever = min(self.forever, self.epoch)
 
 			updated_tasks = ray.get(workers)
 			self.tasks = [task for task, _ in updated_tasks]
 			for task in self.tasks: 
-				self.logs[task.number]['trajectories'].append(task.trajectories)
-				self.energies[task.number].append(task.trajectories[0][-1].reward)
+				self.sliding_window[task.number].append(task.trajectories)
+				self.energy_list[task.number].append(task.trajectories[0][-1].reward)
+				if len(self.sliding_window[task.number]) > self.outer_window_size:
+					self.sliding_window[task.number].pop(0)
+					self.energy_list[task.number].pop(0)
 			# Compute where each task should resume on the next epoch.
 			for i,_ in enumerate(self.resume_state): self.resume_state[i] = updated_tasks[i][1]['resume']
 			
 			self.epoch += 1
 
-		for finalizer in self.finalizers: finalizer(self.epoch_stop_at, self.epoch, self.logs)
+		for finalizer in self.finalizers: finalizer(self.forever, self.epoch, self.sliding_window)
 	def cont(self):
-		return self.epoch < self.epoch_stop_at + self.epoch_additional
+		return self.epoch < self.forever + self.outer_window_size+1
+
+def var(batch):
+	summed = functools.reduce(lambda sum, item: sum + item, batch, 0)
+	squared_sums = functools.reduce(lambda sum, item: sum + item**2, batch,0)
+	return squared_sums/len(batch) - (summed/len(batch))**2
 
 @ray.remote(num_cpus=1)
-def in_equilibrium(epoch, energy_list, lookback_length, eps=2):
+def in_equilibrium(epoch, energy_list, inner_window_size, eps=2):
 	num_tasks = len(energy_list)
-	num_epochs = len(energy_list[0])
-	if  num_epochs < lookback_length: return False
-	else:
-		accumulator = 0
-		for i,j in itertools.product(range(num_tasks), range(num_tasks)):
-			if j<i: continue
-			i, j = energy_list[i][-(lookback_length+1):], energy_list[j][-(lookback_length+1):]
-			accumulator += functools.reduce(lambda sum,item: sum + abs(item[0].double()-item[1].double()), zip(i,j),0)
-		to_beat =  eps * lookback_length * (.5*num_tasks**2)
-		print(accumulator, to_beat)
-		return accumulator < to_beat
+	cm = torch.zeros((num_tasks, num_tasks))	
+
+	# Compute correlation between starting and ending windows for all pairs.
+	var_wni = torch.zeros((num_tasks,))
+	for i,j in itertools.product(range(num_tasks), range(num_tasks)):
+		wni = torch.tensor(energy_list[i][-(inner_window_size+1):])
+		#print(wni)
+		var_wni = var(wni.view(-1))
+		woj = torch.tensor(energy_list[j][:inner_window_size])
+		cm[i,j] = (wni.float().mean()-woj.float().mean())
+	var_cm = var(cm.view(-1))
+	svar_cm = var_cm/num_tasks**2
+	svar_wni = var_wni / num_tasks
+	print(f"vcm={svar_cm}, vwni={svar_wni}, ad = {abs(svar_cm - svar_wni)}")
+	cond = abs(svar_cm - svar_wni) < 1
+	if cond: print("!!!!!!\nI eq'ed\n!!!!!!")
+	return cond
 		
 
 @ray.remote(num_cpus=1)
@@ -133,24 +149,20 @@ def train_ground_search(index, epoch, start_state, task):
 
 		trajectories = trajectories.view(-1)
 		# TODO: Figure out how to remap rewards in a sane fashion.
-		print(f"R^bar_({epoch:04d})_{task.number} = {(sum(trajectories)/len(trajectories)).item():07f}. Best was {min(trajectories):03f}.")
+		#print(f"R^bar_({epoch:04d})_{task.number} = {(sum(trajectories)/len(trajectories)).item():07f}. Best was {min(trajectories):03f}.")
 
 	trainer.run(range(1), max_epochs=1)
 	ret_state = task.trajectories[0][-1].state
 	return task, {"resume":ret_state}
 		
-def end_computation(eq_epoch, ending_epoch, logs):
-	for log in logs:
-		log = logs[log]
-		print(len(log['trajectories']))
+def end_computation(eq_epoch, ending_epoch, sliding_window):
 	print("Finalized!!")
 
-def magnitization(eq_epoch, ending_epoch, task_logs):
+def magnitization(eq_epoch, ending_epoch, sliding_window):
 	summed_mag = 0
-	for task in task_logs:
-		task = task_logs[task]
+	for idx, task in enumerate(sliding_window):
 		mag = 0
-		for buffer in task['trajectories'][-1]:
+		for buffer in sliding_window[idx][-1]:
 			state = buffer[-1].state
 			#print(state)
 			mag = state.float().mean()
@@ -161,38 +173,33 @@ class specific_heat:
 		self.beta = beta
 		self.glass_shape = glass_shape
 
-	def var(self, batch):
-		summed = functools.reduce(lambda sum, item: sum + item, batch, 0)
-		squared_sums = functools.reduce(lambda sum, item: sum + item**2, batch,0)
-		return squared_sums/len(batch) - (summed/len(batch))**2
-		
-	def __call__(self, eq_epoch, ending_epoch, task_logs):
-		num_spins = functools.reduce(lambda prod,item: prod *item, self.glass_shape,1)
-		for task in task_logs:
-			task = task_logs[task]
-			energies, variances = [], []
 
-			for buffers in task['trajectories'][eq_epoch:]:
+		
+	def __call__(self, eq_epoch, ending_epoch, sliding_window):
+		num_spins = functools.reduce(lambda prod,item: prod *item, self.glass_shape,1)
+		for idx, task in enumerate(sliding_window):
+			energies, variances = [], []
+			for buffers in task:
 				for buffer in buffers:
 					energies.extend([buffer[idx].reward for idx in range(len(buffer))])	
-					
+
 			batches = [np.random.choice(energies, len(energies)) for i in range(100)]
 			
 			# Compute per-batch variance
 			for batch in batches:
-				variance = self.var(batch)
+				variance = var(batch)
 				variance *= self.beta**2 / num_spins
 				variances.append(variance)
 
 			# SQRT(AVG(C^2)-AVG(C)^2)
 			# Compute variance of variances, which is specific hea	
-			specific_heat = self.var(energies) * self.beta**2 / num_spins
-			error_c = self.var(variances)**.5
+			specific_heat = var(energies) * self.beta**2 / num_spins
+			error_c = var(variances)**.5
 			# See section 3.4 of online book
 			print(f"C = {specific_heat} ± {error_c}")
 if __name__ == "__main__":
-	glass_shape = (8,8,8)
-	beta = 2.2#1/2.2275
+	glass_shape = (10,10)
+	beta = 10#1/2.2275
 	ctrl = controller.remote(10, beta, glass_shape, [end_computation, magnitization, specific_heat(beta, glass_shape)])
 
 	ray.get(ctrl.run.remote())

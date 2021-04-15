2279c3543005f9330c0a078533b9231328636e83
Matthew McRaven <mkm302@georgetown.edu>
2021-04-15 13:47:24 -0700

    Add sweeping code for 10x10 glasses.

--- anneal/main.py
+++ anneal/main.py
@@ -1,13 +1,20 @@
+import pickle
+
+
 import ray
+import numpy as np
+
 import graphity.pipelines
 
 if __name__ == "__main__":
-	#ray.init(address='auto')
+	vals = np.linspace(.1, 3, 40)
 	glass_shape = (10,10)
-	beta = 2.9
 	task_count = 10
-	tasks = [graphity.pipelines.create_task(idx, beta, glass_shape) for idx in range(task_count)]
-	eq_graphs = graphity.pipelines.sync_evolver(tasks, max_epochs=10000).run()
-	aug_graphs = graphity.pipelines.sync_augmenter(eq_graphs, beta, sweeps=1).run()
-	print(len(aug_graphs))
-	print(f"beta = {beta}")
+	for val in vals:
+		beta = val
+		print(f"beta = {beta}")
+		tasks = [graphity.pipelines.create_task(idx, beta, glass_shape) for idx in range(task_count)]
+		eq_graphs = graphity.pipelines.sync_evolver(tasks, max_epochs=10000).run()
+		aug_graphs = graphity.pipelines.sync_augmenter(eq_graphs, beta, sweeps=1).run()
+		with open(f'beta{round(val, 3)}.pickle', 'wb') as handle:
+			pickle.dump(aug_graphs, handle, protocol=pickle.HIGHEST_PROTOCOL)

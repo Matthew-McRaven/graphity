Fix broken variance computation.

src/graphity/pipelines/evolve.py
@@ -19,11 +19,10 @@ def in_equilibrium(epoch, energy_list, inner_window_size, eps=2):
 	var_wni = torch.zeros((num_tasks,))
 	for i,j in itertools.product(range(num_tasks), range(num_tasks)):
 		wni = torch.tensor(energy_list[i][-(inner_window_size+1):])
-		#print(wni)
-		var_wni[i] = var(wni.view(-1))
+		var_wni[i] = torch.var(wni.view(-1))
 		woj = torch.tensor(energy_list[j][:inner_window_size])
 		cm[i,j] = (wni.float().mean()-woj.float().mean())
-	var_cm = var(cm.view(-1))
+	var_cm = torch.var(cm.view(-1))
 	svar_cm = var_cm / num_tasks ** 2
 	svar_wni = var_wni.mean() / num_tasks
 	#print(f"vcm={svar_cm}, vwni={svar_wni}, ad = {abs(svar_cm - svar_wni)}")

src/graphity/pipelines/stat.py
@@ -27,15 +27,15 @@ class specific_heat:
 			
 			# Compute per-batch variance
 			for batch in batches:
-				variance = var(batch)
+				variance = np.var(batch)
 				variance *= self.beta**2 / num_spins
 				variances.append(variance)
 
 			# SQRT(AVG(C^2)-AVG(C)^2)
 			# Compute variance of variances, which is specific hea	
-			specific_heat = var(energies) * self.beta**2 / num_spins
+			specific_heat = np.var(energies) * self.beta**2 / num_spins
 			c.append(specific_heat)
-			error_c = var(variances)**.5
+			error_c = np.var(variances)**.5
 			# See section 3.4 of online book
 			#print(f"C = {specific_heat} ± {error_c}")
 		return c
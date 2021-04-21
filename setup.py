cc0e7b4b6bac58cca88a4ecf8788c9c47280a86d
Matthew McRaven <mkm302@georgetown.edu>
2021-04-21 13:05:50 -0700

    Fix CI/CD.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = []
 install_reqs.extend(["more-itertools", "overrides", "future", "matplotlib", "scipy", "Pillow"])
 install_reqs.extend(["torch ~= 1.7.0", "torchvision ~= 0.8.1", "pytest ~= 6.1"])
 setup(name='graphity',
-      version='0.3.0',
+      version='0.4.0',
       description='Research in background independent quantum gravity, also known as quantum graphity.',
       author='Matthew McRaven',
       author_email='mkm302@georgetown.edu',

a08cb31fe55cf26eda7eb7d49ce0e7145e639dc5
Matthew McRaven <mkm302@georgetown.edu>
2021-04-21 12:49:13 -0700

    Refactor autocorrelation's Chi.
    Chi is the same in both distributed and single threaded version.
    This changes the code to only have one definition of Chi.

--- src/graphity/pipelines/autocor.py
+++ src/graphity/pipelines/autocor.py
@@ -9,7 +9,16 @@ import ray
 
 import graphity.environment.lattice
 from .aug import *
-
+def chi(t, t_max, trajectory):
+	scale = (1/t_max - t)
+	term_0, term_1, term_2 = torch.tensor(0.),torch.tensor(0.),torch.tensor(0.)
+	for t_prime in range(0, t_max - t):
+		term_0 += trajectory[t_prime]['state'].float().mean() *  trajectory[t_prime+t]['state'].float().mean()
+		term_1 += trajectory[t_prime]['state'].float().mean()
+		term_2 += trajectory[t]['state'].float().mean()
+	ret = scale*term_0 - scale*(term_1 * term_2)
+	assert torch.is_tensor(ret)
+	return ret
 class sync_autocorrelation:
 	def __init__(self, eq_lattices, beta, H=graphity.environment.lattice.IsingHamiltonian(), sweeps=100):
 		self.beta = beta
@@ -22,26 +31,18 @@ class sync_autocorrelation:
 		trajectories = [augment(self.eq_lattices[idx], task, self.sweeps) for idx, task in enumerate(tasks)]
 		autocorrelation_times = []
 		for trajectory in trajectories:
-			first_below_zero = [idx for idx, item in enumerate(trajectory) 
-				if self.chi(idx, self.sweeps, trajectory)/self.chi(0, self.sweeps, trajectory)<0][0]
+			below_zero = [idx for idx, item in enumerate(trajectory) 
+				if chi(idx, self.sweeps, trajectory)<0]#/chi(0, self.sweeps, trajectory)<0]
+			first_below_zero = below_zero[0] if below_zero else self.sweeps
 			if first_below_zero < 1: first_below_zero = torch.tensor([1]) 
-			norm =  self.chi(0, first_below_zero, trajectory)
+			norm =  chi(0, first_below_zero, trajectory)
 			assert torch.is_tensor(norm)
-			act = functools.reduce(lambda sum, t:sum+self.chi(t, first_below_zero, trajectory)/norm, 
+			act = functools.reduce(lambda sum, t:sum+chi(t, first_below_zero, trajectory)/norm, 
 				range(first_below_zero), torch.tensor(0))
 			assert torch.is_tensor(act)
 			autocorrelation_times.append(torch.round(act).type(torch.LongTensor))
 		return max(max(autocorrelation_times), 1)
-	def chi(self, t, t_max, trajectory):
-		scale = (1/t_max - t)
-		term_0, term_1, term_2 = torch.tensor(0.),torch.tensor(0.),torch.tensor(0.)
-		for t_prime in range(0, t_max - t):
-			term_0 += trajectory[t_prime]['state'].float().mean() *  trajectory[t_prime+t]['state'].float().mean()
-			term_1 += trajectory[t_prime]['state'].float().mean()
-			term_2 += trajectory[t]['state'].float().mean()
-		ret = scale*term_0 - scale*(term_1 * term_2)
-		assert torch.is_tensor(ret)
-		return ret
+
 
 class distributed_sync_autocorrelation:
 	def __init__(self, eq_lattices, beta, H=graphity.environment.lattice.IsingHamiltonian(), sweeps=100):
@@ -57,24 +58,13 @@ class distributed_sync_autocorrelation:
 		for trajectory in trajectories:
 			trajectory = ray.get(trajectory)
 			below_zero = [idx for idx, item in enumerate(trajectory) 
-				if self.chi(idx, self.sweeps, trajectory)/self.chi(0, self.sweeps, trajectory)<0]
-			first_below_zero = below_zero[0] if below_zero else self.sweeps	
+				if chi(idx, self.sweeps, trajectory)/chi(0, self.sweeps, trajectory)<0]
+			first_below_zero = below_zero[0] if below_zero else self.sweeps-1	
 			if first_below_zero < 1: first_below_zero = torch.tensor([1]) 
-			norm =  self.chi(0, first_below_zero, trajectory)
+			norm =  chi(0, first_below_zero, trajectory)
 			assert torch.is_tensor(norm)
-			act = functools.reduce(lambda sum, t:sum+self.chi(t, first_below_zero, trajectory)/norm, 
+			act = functools.reduce(lambda sum, t:sum+chi(t, first_below_zero, trajectory)/norm, 
 				range(first_below_zero), torch.tensor(0))
 			assert torch.is_tensor(act)
 			autocorrelation_times.append(torch.round(act).type(torch.LongTensor))
 		return max(max(autocorrelation_times), 1)
-
-	def chi(self, t, t_max, trajectory):
-		scale = (1/t_max - t)
-		term_0, term_1, term_2 = torch.tensor(0.),torch.tensor(0.),torch.tensor(0.)
-		for t_prime in range(0, t_max - t):
-			term_0 += trajectory[t_prime]['state'].float().mean() *  trajectory[t_prime+t]['state'].float().mean()
-			term_1 += trajectory[t_prime]['state'].float().mean()
-			term_2 += trajectory[t]['state'].float().mean()
-		ret = scale*term_0 - scale*(term_1 * term_2)
-		assert torch.is_tensor(ret)
-		return ret

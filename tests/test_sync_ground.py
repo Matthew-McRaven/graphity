fc8ce17f32504fe4a58b1b5f9f4f2dce7b8a47eb
Matthew McRaven <mkm302@georgetown.edu>
2021-04-25 11:26:07 -0700

    Conditionally track minima.

--- tests/test_sync_ground.py
+++ tests/test_sync_ground.py
@@ -11,9 +11,10 @@ def test_sync_eq():
 	x = []
 	print(f"beta = {beta}")
 	tasks = [graphity.pipelines.create_ground_task(idx, beta, glass_shape) for idx in range(task_count)]
-	eq_lattices = graphity.pipelines.sync_evolver(tasks, max_epochs=10, inner_window_size=1, outer_window_size=2,
-		run_fn=graphity.pipelines.run_ground, eq_check_fn=None).run()
-	aug_lattices = graphity.pipelines.sync_augmenter(eq_lattices, beta, sweeps=1).run()
+	ev = graphity.pipelines.sync_evolver(tasks, max_epochs=10, inner_window_size=1, outer_window_size=2,
+		run_fn=graphity.pipelines.run_ground, eq_check_fn=None, track_minima=True)
+	ev.run()
+	print(ev.minima())
 	x.extend(task_count*[beta])
 	print(f"beta = {beta}")
 

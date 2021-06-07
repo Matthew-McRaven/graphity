b3b150908be34571e6d0f374f40ce84a57cdd50e
Matthew McRaven <mkm302@georgetown.edu>
2021-06-07 11:55:21 -0700

    (GRAP-17) Sample edges from binomial distribution.
    This allows us to construct graphs whose edge distributions roughly imitate ground states.

--- src/graphity/environment/graph/generate.py
+++ src/graphity/environment/graph/generate.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy.random import default_rng
 import torch.tensor
 
-def random_graph(graph_size, rng=None):
+def random_graph(graph_size, rng=None, p=.5):
     """
     Create a random directed graph of a given size.
 
@@ -15,11 +15,11 @@ def random_graph(graph_size, rng=None):
         # Need to generate a random nXn graph that is square, symmetric, integer-valued, and all 1's or 0's.
         rng = default_rng()
     # RNG excludes hi endpoint.
-    rand = rng.integers(0,2, (graph_size, graph_size))
+    rand = rng.binomial(1, p=p, size=(graph_size, graph_size))
 
     return torch.tensor(rand)
 
-def random_adj_matrix(graph_size, allow_self_loops=False, rng=None):
+def random_adj_matrix(graph_size, allow_self_loops=False, rng=None, p=.5):
     """
     Create a random undirected graph.
 
@@ -33,7 +33,7 @@ def random_adj_matrix(graph_size, allow_self_loops=False, rng=None):
         # Need to generate a random nXn graph that is square, symmetric, integer-valued, and all 1's or 0's.
         rng = default_rng()
     # RNG excludes hi endpoint.
-    rand = random_graph(graph_size, rng)
+    rand = random_graph(graph_size, rng, p)
     # Mask out all values on or above the diagonal
     lower_tril = np.tril(rand, -1)
     # TODO: Explain magic of why transpose works.

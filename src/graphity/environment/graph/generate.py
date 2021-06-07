449b420bf03558e0a64b2bfa05a82817a888242d
Matthew McRaven <mkm302@georgetown.edu>
2021-06-07 14:31:29 -0700

    (GRAP-21) Add code to generate random pure graphs.
    Must be extended in order to exhaustively create all pure graphs.

--- src/graphity/environment/graph/generate.py
+++ src/graphity/environment/graph/generate.py
@@ -1,6 +1,10 @@
+import random
+
+import networkx as nx
 import numpy as np
 from numpy.random import default_rng
 import torch.tensor
+
 
 def random_graph(graph_size, rng=None, p=.5):
     """
@@ -18,6 +22,54 @@ def random_graph(graph_size, rng=None, p=.5):
     rand = rng.binomial(1, p=p, size=(graph_size, graph_size))
 
     return torch.tensor(rand)
+
+def random_pure_graph(maximal_clique_size, graph_size):
+    """
+    Create a random pure graph.
+
+    The algorithm for growing random pure graphs is as follows:
+    1) Start with a complete graph of size `maximal_clique_size`.
+    2) Create a list which contains all maximal cliques.
+    3) While there are insufficient nodes in the graph:
+        a) Randomly choose a clique from the list of maximal cliques.
+        b) Randomly remove one node from the selected clique.
+        c) Add a new node to the graph, and add edges between it an all reamining graphs in the selected clique.
+           The new node now has the correct maximal clique size (it's connected to k-1 nodes that are all connected to eachother).
+           The old nodes maximal clique size is unchanged, since no new connections between existing nodes were added.
+        d) Add the new clique to the maximal clique list.
+
+    :param maximal_clique_size: The max clique size of the pure graph.
+    :param graph_size: The total number of nodes in the output graph.
+    """
+    # It is impossible to generate a graph with fewer than clique_size number of nodes.
+    assert(graph_size >= maximal_clique_size)
+
+    # Generate a complete graph.
+    G = nx.complete_graph(maximal_clique_size)
+    # Get a list of all maximal cliques, which is just the set of all nodes.
+    cliques = list(nx.find_cliques(G))
+
+    # Extend the graph until we hit the desired number of nodes.
+    for i in range(graph_size-maximal_clique_size):
+
+        # Select a random clique to be used as a base for the new clique.
+        base_clique = random.choice(cliques)
+        # Select a random node to remove from the existing clique.
+        # This prevents the maximal clique size from growing.
+        random.shuffle(base_clique)
+        chopped_clique = base_clique[:-1]
+
+        # Add a new node and connect it to all remaining nodes in the clique.
+        new_node = len(G)+1
+        G.add_node(new_node)
+        G.add_edges_from([(new_node, i) for i in chopped_clique])
+
+        # Create (the only) clique involving the new node.
+        new_clique = chopped_clique+[new_node]
+
+        # Append the newly created clique to the list of cliques.
+        cliques.append(new_clique)
+    return torch.tensor(nx.to_numpy_array(G))
 
 def random_adj_matrix(graph_size, allow_self_loops=False, rng=None, p=.5):
     """

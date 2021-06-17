1b1a918eac272a797a61cdafe553ca9663b8ac6c
Matthew McRaven <matthew.mcraven@gmail.com>
2021-06-17 11:41:28 -0700

    Add random fit as baseline.

---
+++ tools/learn/1_fit_rng.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+from pathlib import Path
+import random
+
+import graphity.data
+from graphity.environment.graph import *
+
+
+
+def main(args):
+	graph_size, clique_size = args.graph_size, args.clique_size
+	terms = []
+	terms.append(ACoef(graph_size, rows=4, cols=2))
+	terms.append(FACoef(graph_size, rows=4, cols=2))
+	net = SumTerm(graph_size, terms)
+
+	# Save items to disk
+	parent = Path("data/models/rng/")
+	parent.mkdir(parents=True, exist_ok=True)
+	if(os.path.exists(parent/f"({clique_size}-{graph_size}).pth")): os.remove(parent/f"({clique_size}-{graph_size}).pth")
+	torch.save(net, parent/f"({clique_size}-{graph_size}).pth")
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Train a NN to recognize graphs')
+	parser.add_argument('-g', '--graph_size', required=True, type=int, help='The number nodes in each generated graph.')
+	parser.add_argument('-k', '--clique_size', required=True, type=int, help='The maximal clique size in each generated graph.')
+	args = parser.parse_args()
+	main(args)

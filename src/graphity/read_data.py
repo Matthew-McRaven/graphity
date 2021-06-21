34a97d8d8701e543f5cf6707249038a25c83c57d
Matthew McRaven <mkm302@georgetown.edu>
2021-06-21 14:50:09 -0700

    Document read_data.

--- src/graphity/read_data.py
+++ src/graphity/read_data.py
@@ -1,6 +1,10 @@
 import torch
 
 def read_betre_format(filename):
+	"""
+	Read a file stored in the format of a file in data/ground.
+	These files came from Betre in Dec 2020, and represent spherical/geometric graphs of various sizes.
+	"""
 	ret_tens = []
 	with open(filename) as  data_file:
 		for line in data_file.readlines():

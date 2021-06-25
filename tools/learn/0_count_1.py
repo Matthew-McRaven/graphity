1c0c449625a5a2197545553a52632937e5a5001f
Matthew McRaven <mkm302@georgetown.edu>
2021-06-25 15:49:03 -0700

    Show cumulative statistics.
    Bins are too small to see on their own.

--- tools/learn/0_count_1.py
+++ tools/learn/0_count_1.py
@@ -15,12 +15,13 @@ def main(args):
 	pure_dir = f"data/pure/({clique_size}-{graph_size})"
 	dataset = graphity.data.FileGraphDataset(pure_dir, None)
 	_1s = [x[0].mean().item() for x in dataset]
-	plt.hist(_1s, bins = 20)
-	plt.xlabel("Probability of edge")
+	plt.hist(_1s, bins = 1000, density=True, cumulative=True)
+	plt.xlabel("Cumulative probability of edge")
 	plt.suptitle(f"k={clique_size}, g={graph_size}")
 	plt.ylabel("Relative liklihood of each prob.")
 	lb, ub = graphity.data.bound_impure(clique_size, graph_size)
-	plt.vlines((lb, ub), 0, 100, "red", alpha=.25)
+	plt.axvline(lb, color="red", alpha=.25)
+	plt.axvline(ub, color="red", alpha=.25)
 	plt.savefig(f"pure-({clique_size}-{graph_size})-edges")
 	plt.show()
 

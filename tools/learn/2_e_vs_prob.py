5d8ced104a46353e30f4d93c3a0c05fb186be15e
Matthew McRaven <mkm302@georgetown.edu>
2021-07-01 16:53:16 -0700

    Plot im/pure graphs.

--- tools/learn/2_e_vs_prob.py
+++ tools/learn/2_e_vs_prob.py
@@ -29,11 +29,23 @@ def aug(data, labels):
 	return to_add
 def eval(H, dataset, clique_size, graph_size):
 	clf = svm.SVC()
-	data, target = [], []
+	data_p, target_p = [], []
+	data_i, target_i = [], []
 	for value, label in dataset:
-		data.append(value.float().mean().detach().cpu().numpy())
-		target.append(H(value).detach().cpu().numpy())
-	plt.scatter(data, target, alpha=0.01)
+		if label == 0:
+			data_p.append(value.float().mean().detach().cpu().numpy())
+			target_p.append(H(value).detach().cpu().numpy())
+		elif label == 1:
+			data_i.append(value.float().mean().detach().cpu().numpy())
+			target_i.append(H(value).detach().cpu().numpy())
+	plt.scatter(data_p, target_p, alpha=0.01, color="red")
+	plt.scatter(data_i, target_i, alpha=0.01, color="blue")
+	lb, ub = graphity.data.bound_impure(clique_size, graph_size)
+	plt.axvline(lb, color="red", alpha=.25)
+	plt.axvline(ub, color="red", alpha=.25)
+	plt.xlabel("E|G|")
+	plt.suptitle(f"Edge Probability vs Energy")
+	plt.ylabel("H(G)")
 	plt.savefig(f"e-vs-prob-{clique_size,graph_size}.png")
 
 def main(args):
@@ -42,14 +54,15 @@ def main(args):
 	# Load item from disk
 	parent = Path(args.H)
 	path = parent/f"({clique_size}-{graph_size}).pth"
-	model = torch.load(path.absolute())
+	model = torch.load(path.absolute()).to("cpu")
 	model.eval()
 	
 
 	pure_dir = f"data/pure/({clique_size}-{graph_size})"
+	impure_dir = f"data/impure/({clique_size}-{graph_size})"
 	if not os.path.exists(pure_dir): 
 		raise ValueError("Dataset directories must exist. Please run 0_generate.py with the same g,k.")
-	dataset = graphity.data.FileGraphDataset(pure_dir, "")
+	dataset = graphity.data.FileGraphDataset(pure_dir, impure_dir)
 	eval(model, dataset, clique_size, graph_size)
 
 

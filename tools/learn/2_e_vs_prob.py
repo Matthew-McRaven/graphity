374c2051c507a5b758d02f42351a6291773ef982
Matthew McRaven <mkm302@georgetown.edu>
2021-07-08 12:38:43 -0700

    Split impure/pure plots.
    This makes it easier to see trends within a grouping,
    Then, superimpose the datasets to see total trends.
    
    Must separate pure / impure graphs via a loop.

--- tools/learn/2_e_vs_prob.py
+++ tools/learn/2_e_vs_prob.py
@@ -29,20 +29,37 @@ def aug(data, labels):
 	return to_add
 def eval(H, dataset, clique_size, graph_size):
 	clf = svm.SVC()
-	data, target, color = [], [], []
+	d_p, t_p, c_p = [], [], []
+	d_i, t_i, c_i = [], [], []
 	for value, label in dataset:
-		data.append(value.float().mean().detach().cpu().numpy())
-		target.append(H(value).detach().cpu().numpy())
-		color.append(graphity.utils.purity_degree(value, clique_size))
-	plt.scatter(data, target, c=color, alpha=0.1, cmap='coolwarm', vmin=0, vmax=1)
+		if label == 1.0:
+			d_p.append(value.float().mean().detach().cpu().numpy())
+			t_p.append(H(value).detach().cpu().numpy())
+			c_p.append(graphity.utils.purity_degree(value, clique_size))
+		else:
+			d_i.append(value.float().mean().detach().cpu().numpy())
+			t_i.append(H(value).detach().cpu().numpy())
+			c_i.append(graphity.utils.purity_degree(value, clique_size))
+
+	f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, sharex=True)
+	ax1.scatter(d_i, t_i, c=c_i, alpha=0.1, cmap='coolwarm', vmin=0, vmax=1)
+	ax1.set_title("Impure Graphs")
+	ax2.scatter(d_p, t_p, c=c_p, alpha=0.1, cmap='coolwarm', vmin=0, vmax=1)
+	ax2.set_title("Pure Graphs")
+	ax3.scatter(d_i, t_i, c=c_i, alpha=0.0425, cmap='coolwarm', vmin=0, vmax=1)
+	ax3.scatter(d_p, t_p, c=c_p, alpha=0.1, cmap='coolwarm', vmin=0, vmax=1)
+	ax3.set_title("All Graphs")
 	lb, ub = graphity.data.bound_impure(clique_size, graph_size)
-	plt.axvline(lb, color="red", alpha=.25)
-	plt.axvline(ub, color="red", alpha=.25)
-	plt.xlabel("E|G|")
-	clb = plt.colorbar()
-	clb.ax.set_title("Degree of Purity")
-	plt.suptitle(f"Edge Probability vs Energy")
-	plt.ylabel("H(G)")
+	ax1.axvline(lb, color="red", alpha=.25), ax1.axvline(ub, color="red", alpha=.25)
+	ax2.axvline(lb, color="red", alpha=.25), ax2.axvline(ub, color="red", alpha=.25)
+	ax3.axvline(lb, color="red", alpha=.25), ax3.axvline(ub, color="red", alpha=.25)
+	f.supxlabel("E|G|")
+	clb = f.colorbar(plt.cm.ScalarMappable(cmap=plt.get_cmap('coolwarm')), pad=.15)
+	clb.ax.set_title("Purity Ratio")
+	f.suptitle(f"Edge Probability vs Energy")
+	f.supylabel("H(G)")
+	f.set_size_inches(8, 6)
+	plt.tight_layout()
 	plt.savefig(f"e-vs-prob-{clique_size,graph_size}.png")
 
 def main(args):
